6e5778e2021e31909c3533a6f745623acf1d2214
Yuva0 <senguptatanuj@gmail.com>
2022-05-01 17:00:41 +0530

    multer removed

--- api/index.js
+++ api/index.js
@@ -2,10 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
 const postRoute = require("./routes/posts");
-const multer = require("multer");
-
 const cors = require("cors");
 
 dotenv.config();
@@ -17,22 +14,6 @@ mongoose
   .then(() => console.log('Database Connected'))
   .catch(err => console.log(err));
 
-
-app.use(cors());
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, "hello.png");
-  }
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("coverImage"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
 
 app.use("/api/posts", postRoute);
 
