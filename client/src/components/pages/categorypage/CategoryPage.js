80bb7a95c8bc491a206c83fe9b3ad8adbcd85e9b
Yuva0 <senguptatanuj@gmail.com>
2022-05-04 17:48:11 +0530

    card implemented

--- client/src/components/pages/categorypage/CategoryPage.js
+++ client/src/components/pages/categorypage/CategoryPage.js
@@ -1,18 +1,34 @@
 import { useParams } from 'react-router-dom';
+import { useEffect,useState } from 'react';
 
 import classes from './css/CategoryPage.module.css';
 
 // import MediumSet from '../../mediumset/MediumSet';
 import AllSet from '../../allset/AllSet';
 import CoverPage from '../../coverpage/CoverPage';
+import CardMediumSet from '../../cardmediumset/CardMediumSet';
 
-const CategoryPage = (props) => {
+const CategoryPage = () => {
   const params = useParams();
+  const [content,setContent] = useState();
+
+  useEffect(() => {
+      if(params.category === "philosophy"){
+        setContent(<CardMediumSet category="philosopher" title="Greatest Philosophers of All Time!"/>);
+      }
+      else if(params.category === "mindset"){
+      }
+      return () => { 
+        setContent(); 
+      };
+  },[params.category])
+
   return (
     <div className={classes.categorypage}>
       <CoverPage/>
       {/* <MediumSet title="Latest Articles" time="latest" type="article" category={params.category}/> */}
       <AllSet title="All Articles" time="latest" type="article" category={params.category}/>
+      {content}
     </div>
   );
 };
